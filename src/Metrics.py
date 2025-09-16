be770badeeee746791c30e0b144047af9fdf3a95
nwisnie <nwisnie@purdue.edu>
2025-09-16 18:15:34 -0400

    license metric draft

--- src/Metrics.py
+++ src/Metrics.py
@@ -61,3 +61,159 @@ class Metric(ABC):
             A score between 0.0 and 1.0.
         """
         raise NotImplementedError
+    
+    # src/license_metric.py
+# THIS CODE WILL HANDLE THE LICENSE METRIC.
+from Metrics import Metric, MetricResult
+# from huggingface_hub import HfApi
+from Client import HFClient
+from typing import Any
+import time
+import os
+
+
+class LicenseMetric(Metric):
+    """
+    Checks the license type of a Hugging Face model
+    using HF Hub metadata.
+    Returns a score based on license_scores dict.
+    """
+    # Based on:
+    # linking / distribution /
+    # modification / patent grant /
+    # private use and sublicensing
+    # (how wikipedia categorizes them)
+    license_scores: dict[str, float] = {
+        # Permissive
+        "apache-2.0": 1.0,
+        "mit": 1.0,
+        "afl-3.0": 1.0,
+        "bsd": 1.0,
+        "bsd-2-clause": 1.0,
+        "bsd-3-clause": 1.0,
+        "bsd-3-clause-clear": 1.0,
+        "isc": 1.0,
+        "zlib": 1.0,
+        "cdla-permissive-1.0": 1.0,
+        "cdla-permissive-2.0": 1.0,
+        "ms-pl": 1.0,
+        "postgresql": 1.0,
+        "osl-3.0": 1.0,
+        "apple-ascl": 1.0,
+        "mpl-2.0": 1.0,
+        "pddl": 1.0,
+        "unlicense": 1.0,
+        "cc0-1.0": 1.0,
+        "wtfpl": 1.0,
+        "intel-research": 1.0,
+        "ofl-1.1": 1.0,
+        "lppl-1.3c": 1.0,
+        "ncsa": 1.0,
+        "etalab-2.0": 1.0,
+
+        # Less permissive
+        "gpl-3.0": 0.75,
+        "gpl-2.0": 0.75,
+        "gpl": 0.75,
+        "agpl-3.0": 0.75,
+        "lgpl-3.0": 0.75,
+        "lgpl-2.1": 0.75,
+        "lgpl": 0.75,
+        "lgpl-lr": 0.75,
+        "epl-2.0": 0.75,
+        "epl-1.0": 0.75,
+        "ecl-2.0": 0.75,
+        "eupl-1.1": 0.75,
+        "eupl-1.2": 0.75,
+        "artistic-2.0": 0.75,
+        "cdla-sharing-1.0": 0.75,
+        "cc-by-4.0": 0.75,
+        "cc-by-3.0": 0.75,
+        "cc-by-2.0": 0.75,
+        "cc-by-2.5": 0.75,
+        "cc-by-sa-4.0": 0.75,
+        "cc-by-sa-3.0": 0.75,
+        "odc-by": 0.75,
+        "bsl-1.0": 0.75,
+        "odbl": 0.75,
+        "gfdl": 0.75,
+
+        # Restrictive
+        "cc-by-nc-4.0": 0.5,
+        "cc-by-nc-2.0": 0.5,
+        "cc-by-nc-3.0": 0.5,
+        "cc-by-nc-nd-4.0": 0.5,
+        "cc-by-nc-nd-3.0": 0.5,
+        "cc-by-nc-sa-4.0": 0.5,
+        "cc-by-nc-sa-3.0": 0.5,
+        "cc-by-nc-sa-2.0": 0.5,
+        "cc-by-nd-4.0": 0.5,
+        "fair-noncommercial-research-license": 0.5,
+
+        # Model-specific / special licenses
+        "openrail": 0.75,
+        "creativeml-openrail-m": 0.75,
+        "openrail++": 0.75,
+        "gemma": 0.75,
+        "llama2": 0.75,
+        "llama3": 0.75,
+        "llama3.1": 0.75,
+        "llama3.2": 0.75,
+        "llama3.3": 0.75,
+        "llama4": 0.75,
+        "bigscience-bloom-rail-1.0": 0.75,
+        "bigscience-openrail-m": 0.75,
+        "bigcode-openrail-m": 0.75,
+        "open-mdw": 1,  # open data / model weights, permissive
+        "h-research": 0.5,  # research-only, restrictive
+        "c-uda": 0.5,  # non-commercial, restrictive
+        "apple-amlr": 0.5,  # apple-specific, restrictive
+        "deepfloyd-if-license": 0.5,  # non-commercial research only
+        "cc": 0.75,  # attribution-required
+
+        # maybe we should use an LLM for these guys, not sure yet
+        "other": 0.5  # catch-all, typically restrictive until specified
+    }
+
+    def compute(self, inputs: dict[str, Any], **kwargs) -> float:
+        start = time.time()
+        model_id = inputs.get("model_id")
+        # api = HfApi()
+        # client = HFClient(max_requests=10, token="HF_TOKEN")
+        # model_info = client.request("GET", f"/api/models/{model_id}")
+        error = None
+        token = os.getenv("HF_TOKEN")
+        client = HFClient(max_requests=10, token=token)
+
+        try:
+            # model_info = api.model_info(model_id, files_metadata=True)
+            model_info = client.request("GET", f"/api/models/{model_id}")
+            card_data = model_info.get("cardData", {})
+            license_type = card_data["license"]
+
+            if license_type is None:
+                score = 0.0
+                error = "Liscense not in card data"
+            elif license_type not in self.license_scores:
+                score = 0.0
+                error = "License not found in dict"
+            else:
+                score = self.license_scores[license_type]
+
+        except Exception as e:
+            license_type = None
+            score = 0.0
+            error = str(e)
+
+        latency_ms = (time.time() - start) * 1000
+
+        self.result = MetricResult(
+            metric="License Check",
+            key="license",
+            value=score,
+            latency_ms=latency_ms,
+            details={"license_type": license_type, "model_id": model_id},
+            error=error
+        )
+
+        return score
