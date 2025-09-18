87c719d4f215b0a737f3913873a04e4d72aa2561
Giovanni De Geronimo <gdegeron@purdue.edu>
2025-09-18 12:18:28 -0400

    Fixed problematic line

--- src/CLIApp.py
+++ src/CLIApp.py
@@ -7,4 +7,3 @@ from src.Parser import Parser
 if __name__ == "__main__":
     parse = Parser(sys.argv[1])
     print(f"Parser groups: {parse.getGroups()}")
-    print(f"Model URL:  {parse.getGroups()["model_url"]}")
