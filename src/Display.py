4e4a858ec1ae50d9ec0b5d0c02a01b15792a4214
Giovanni De Geronimo <gdegeron@purdue.edu>
2025-09-27 19:48:08 -0400

    fd

--- src/Display.py
+++ src/Display.py
@@ -126,7 +126,7 @@ def build_output_object(group: Dict[str, str], results: List[MetricResult]) \
     out["dataset_and_code_score_latency"] = avail_lat
     out["dataset_quality"] = dquality_val
     out["dataset_quality_latency"] = dquality_lat
-    out["code_quality"] = 0.8
+    out["code_quality"] = cquality_val
     out["code_quality_latency"] = cquality_lat
     logger.debug("Output object ready: %s", out)
     return out
